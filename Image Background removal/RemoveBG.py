6f0ade983e95147de1004cce102136f8304c1c86
abhishekbhatia08 <46483472+abhishekbhatia08@users.noreply.github.com>
2021-05-03 15:10:19 +0530

    Update RemoveBG.py

--- Image Background removal/RemoveBG.py
+++ Image Background removal/RemoveBG.py
@@ -3,12 +3,12 @@ import requests
 
 response = requests.post(
     'https://api.remove.bg/v1.0/removebg',
-    files={'image_file': open('/Users/abhishek/Dropbox/My Mac (ADMINs-MacBook-Pro.local)/Desktop/Image Background removal/Earth.jpg', 'rb')},
+    files={'image_file': open('(Image Path)', 'rb')},
     data={'size': 'auto'},
     headers={'X-Api-Key': 'ivB2XBqXxUu4ekNmyjWC4uHn'},
 )
 if response.status_code == requests.codes.ok:
-    with open('no-bgpic.png', 'wb') as out:
+    with open('(Output Image Name).png', 'wb') as out:
         out.write(response.content)
 else:
     print("Error:", response.status_code, response.text)
